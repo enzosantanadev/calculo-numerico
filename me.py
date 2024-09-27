1a7c9fc162a10e1fa68f2856e8b1d1984e7a1ec3
Enzo Santana <161235284+enzosantanadev@users.noreply.github.com>
2024-09-27 20:28:37 -0300

    Update me.py

--- me.py
+++ me.py
@@ -10,30 +10,6 @@ G = 9.81
 PRECISAO = 10 ** (-10)
 
 
-def ponto_medio(a, b):
-    return (a+b)/2
-
-
-def modulo(num):
-    if num < 0:
-        return num * -1
-    return num
-
-
-def bolzano(FUNCAO, a, b):
-    return FUNCAO(a) * FUNCAO(b) < 0
-
-
-def bisseccao(FUNCAO, a, b):
-    pm = ponto_medio(a, b)
-    print(pm)
-    if modulo(FUNCAO(pm)) <= PRECISAO:
-        return pm
-
-    if bolzano(FUNCAO, a, pm):
-        return bisseccao(FUNCAO, a, pm)
-    return bisseccao(FUNCAO, pm, b)
-
 
 def FUNCAO(t):
     if t <= 0 or 160000 - 2600 * t <= 0:
@@ -41,8 +17,57 @@ def FUNCAO(t):
     return -750 / (1800 * log(160000 / (160000 - 2600 * t))) / (9.81 * t)
 
 
-print(bisseccao(FUNCAO, 0.1, 100))
+def DERIVADA_FUNCAO(t):
+    if t <= 0 or 160000 - 2600 * t <= 0:
+        return float('inf')
 
+    C = -750 / (1800 * 9.81)
+
+    log_term = log(160000 / (160000 - 2600 * t))
+    d_log_term = (2600 / (160000 - 2600 * t)) / (160000 / (160000 - 2600 * t))
+
+    v = t * log_term
+    v_derivada = log_term + t * d_log_term
+
+    return C * (-v_derivada / (v ** 2))
+
+
+class Newton:
+    def MODULO(self, n):
+        if n < 0:
+            return n * -1
+        return n
+
+    def formula(self, FUNCAO, DERIVADA_FUNCAO, x):
+        return x - (FUNCAO(x) / DERIVADA_FUNCAO(x))
+
+    def metodo(self, FUNCAO, DERIVADA_FUNCAO, CHUTE):
+        if self.MODULO(FUNCAO(CHUTE)) < PRECISAO:
+            return CHUTE
+
+        return self.metodo(FUNCAO, self.formula(FUNCAO, DERIVADA_FUNCAO, CHUTE))
+
+
+class Bisseccao:
+    def ponto_medio(a, b):
+        return (a + b) / 2
+
+    def modulo(self, num):
+        if num < 0:
+            return num * -1
+        return num
+
+    def bolzano(self, FUNCAO, a, b):
+        return FUNCAO(a) * FUNCAO(b) < 0
+
+    def metodo(self, FUNCAO, a, b):
+        pm = self.ponto_medio(a, b)
+        if self.modulo(FUNCAO(pm)) <= PRECISAO:
+            return pm
+
+        if self.bolzano(FUNCAO, a, pm):
+            return self.metodo(FUNCAO, a, pm)
+        return self.metodo(FUNCAO, pm, b)
 
 
 
